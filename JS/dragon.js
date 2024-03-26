dddef2bd07b4b8a731fef1e17ccf5a4c7b5a3add
JOHN T GAUSE III <johntgause12@gmail.com>
2024-03-26 03:11:11 -0700

    mark down walhburg

--- JS/dragon.js
+++ JS/dragon.js
@@ -24,18 +24,37 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 // more
-function goStore (){
-    text.innerText = "You enter the store."
+function goTown() {
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
     button3.innerText = "Go to town square";
     button1.onclick = buyHealth;
     button2.onclick = buyWeapon;
     button3.onclick = goTown;
-}
-function goCave(){
-    console.log("Going to cave.")
-}
-function fightDragon(){
-    console.log("Fighting dragon.")
-}
+    text.innerText = "You enter the store.";
+  }
+  function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You enter the store.";
+  }
+  
+  function goCave() {
+    console.log("Going to cave.");
+  }
+  
+  function fightDragon() {
+    console.log("Fighting dragon.");
+  }
+  
+  function buyHealth() {
+  
+  }
+  
+  function buyWeapon() {
+  
+  }
--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Dragon-Game
+# Dragon Game
+
+- **Authors**: John Gause
+- **Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## 🚀 Overview
+
+- **Placeholder**
+
+## 🛠️ Getting Started
+
+To run this application on your machine, follow these steps:
+
+- ****: 
+- ****: 
+- ****: 
+- ****: 
+
+## 🏗️ Architecture
+
+This application is built using the following technologies and libraries:
+
+-  
+- 
+- 
+- 
+- 
+
+## 🔄 Change Log
+
+Stay up-to-date with the latest enhancements and improvements made to this project:
+
+- **PLACEHOLDER**: [Timestamp] - Description of change.
+
+## 🕒 Estimates
+
+Plan your workflow efficiently with accurate time estimates for project completion:
+
+- **PLACEHOLDER**: [Estimated time for completion]
+
+## 🤝 Credit and Collaborations
+
